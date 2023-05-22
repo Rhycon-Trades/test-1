7cab1ada0a33a8c69d341b5b15899d10275a2a0f
HoussemDegachi <houssemdegach@gmail.com>
2023-05-22 14:27:17 +0100

    V2.06

--- src/compnents/Channels.jsx
+++ src/compnents/Channels.jsx
@@ -1,94 +1,149 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { useEffect, useRef, useState } from 'react'
-import Message from '../ui/Message'
-import { db } from '../firebase/init'
-import { addDoc, collection, limit, onSnapshot, orderBy, query, serverTimestamp, where } from 'firebase/firestore'
-import { useInView } from 'react-intersection-observer'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useRef, useState } from "react";
+import Message from "../ui/Message";
+import { db } from "../firebase/init";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+  where,
+} from "firebase/firestore";
+import { useInView } from "react-intersection-observer";
 
-function Channels({ user , channel }) {
-    const [messages , setMessages] = useState(null)
-    const [newMessage , setNewMessage] = useState()
-    const [scrollToBottom , setScrollToBottom] = useState(true)
-    const [messageSent , setMessageSent] = useState(true)
-    const input = document.getElementById('channel__input') 
-    const dummy = useRef()
-    const {ref , inView} = useInView()
-    
-    useEffect(() => {
-        if(messages && scrollToBottom){
-            dummy.current.scrollIntoView()
-            setScrollToBottom(false)
-        }
-    }, [messages])
+function Channels({ user, channel }) {
+  const [messages, setMessages] = useState(null);
+  const [newMessage, setNewMessage] = useState();
+  const [scrollToBottom, setScrollToBottom] = useState(true);
+  const [messageSent, setMessageSent] = useState(true);
+  const [replyMessage, setReplyMessage] = useState(null);
+  const input = document.getElementById("channel__input");
+  const dummy = useRef();
+  const { ref, inView } = useInView();
 
-    useEffect(() => {
-        if(newMessage && (newMessage.userId === user.uid || inView)){
-            dummy.current.scrollIntoView({ behavior: 'smooth' })
-        }
-    },[newMessage])
-
-    useEffect(() => {
-        if(db){
-            importData()
-        }
-    }, [db])
-
-    useEffect(() => {
-        setMessages(null)
-        importData()
-    },[channel])
-
-    function importData(){
-        const q = query(collection(db, "messages"), where("channel", "==", channel) , orderBy("createdAt") , limit(100));
-        const unsubscribe = onSnapshot(q, (querySnapshot) => {
-          const data = [];
-          querySnapshot.forEach((doc) => {
-            const newData = {...doc.data() , id:doc.id}
-              data.push(newData);
-              setNewMessage(newData)
-          });
-          setMessages(data);
-        });
+  useEffect(() => {
+    if (messages && scrollToBottom) {
+      dummy.current.scrollIntoView();
+      setScrollToBottom(false);
     }
-    
-    function sendMessage(event){
-        event.preventDefault()
-        const message = event.target[0].value
-        if(message != false){
-        const post = {
-            text: message,
-              userName: user.displayName,
-            photoUrl: user.photoURL,
-            userId: user.uid,
-            createdAt: serverTimestamp(), 
-            channel: channel,
-        }
-        addDoc(collection(db , 'messages') , post)
-        setMessageSent(!messageSent)
-        input.value = ''
+  }, [messages]);
+
+  useEffect(() => {
+    if (newMessage && (newMessage.userId === user.uid || inView)) {
+      dummy.current.scrollIntoView({ behavior: "smooth" });
     }
+  }, [newMessage]);
+
+  useEffect(() => {
+    if (db) {
+      importData();
     }
+  }, [db]);
+
+  useEffect(() => {
+    setMessages(null);
+    importData();
+  }, [channel]);
+
+  function importData() {
+    const q = query(
+      collection(db, "messages"),
+      where("channel", "==", channel),
+      orderBy("createdAt"),
+      limit(100)
+    );
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const data = [];
+      querySnapshot.forEach((doc) => {
+        const newData = { ...doc.data(), id: doc.id };
+        data.push(newData);
+        setNewMessage(newData);
+      });
+      setMessages(data);
+    });
+  }
+
+  function sendMessage(event) {
+    event.preventDefault();
+    const message = event.target[0].value;
+    if (message != false) {
+      let replyTo
+      if(replyMessage){
+        replyTo = replyMessage.id
+        setReplyMessage(null)
+      }else{
+        replyTo = null
+      }
+      const post = {
+        text: message,
+        userName: user.displayName,
+        photoUrl: user.photoURL,
+        userId: user.uid,
+        createdAt: serverTimestamp(),
+        channel: channel,
+        replyTo: replyTo
+      };
+      addDoc(collection(db, "messages"), post);
+      setMessageSent(!messageSent);
+      input.value = "";
+    }
+  }
+
+  async function replyTo(message) {
+    setReplyMessage(message);
+  }
 
   return (
-    <div className='channel'>
-        <div className="channel--messages">
+    <div className="channel">
+      <div className="channel--messages">
         <div className="channel--intro">
-        <h2 className="channel__header">Welcome to {channel}</h2>
-        <p className="channel__para">this is the start of this channel</p>
+          <h2 className="channel__header">Welcome to {channel}</h2>
+          <p className="channel__para">this is the start of this channel</p>
         </div>
         <ul className="channel--messages-wrapper">
-            {messages && messages.map((message) => <Message user={user} message={message} key={message.id} />)}
-            <div ref={dummy} ><div ref={ref}></div></div>
+          {messages &&
+            messages.map((message) => (
+              <Message
+                user={user}
+                message={message}
+                replyTo={replyTo}
+                key={message.id}
+              />
+            ))}
+          <div ref={dummy}>
+            <div ref={ref}></div>
+          </div>
         </ul>
+      </div>
+      <form onSubmit={(event) => sendMessage(event)} className="channel__form">
+        <input
+          autoComplete="off"
+          placeholder="Message"
+          type="text"
+          id="channel__input"
+          className="channel__input"
+        />
+        <button type="submit" className="channel__submit">
+          <FontAwesomeIcon icon="fa fa-paper-plane" />
+        </button>
+          {replyMessage !== null && (
+        <div className="channel--reply">
+            <p className="channel--reply__text">
+              Replying to {replyMessage.userName}
+            </p>
+            <button onClick={() => {setReplyMessage(null)}} className="channel--reply__cancel">
+                <FontAwesomeIcon icon='fa fa-xmark' />
+            </button>
         </div>
-            <form onSubmit={(event) => sendMessage(event)} className='channel__form'>
-                <input autoComplete="off" type="text" id='channel__input' className='channel__input'/>
-                <button type="submit" className='channel__submit' >
-                    <FontAwesomeIcon icon='fa fa-paper-plane' />
-                </button>
-            </form>
+          )}
+      </form>
     </div>
-  )
+  );
 }
 
-export default Channels
+export default Channels;
--- src/ui/Message.jsx
+++ src/ui/Message.jsx
@@ -1,64 +1,134 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 import { db } from "../firebase/init";
-import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { deleteDoc, doc, getDoc, updateDoc } from "firebase/firestore";
+import { useEffect } from "react";
 
-function Message({ message, user }) {
-  const [edit , setEdit] = useState(false)
-  const [msgText , setMsgText] = useState(message.text)
+function Message({ message, user, replyTo }) {
+  const [edit, setEdit] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const [reply, setReply] = useState(false);
+  const [msgText, setMsgText] = useState(message.text);
 
-  async function deleteMessage(){
-    const docRef = doc(db , 'messages' , message.id)
-    await deleteDoc(docRef)
+  useEffect(() => {
+    if (message.replyTo) {
+      getReply();
+    }
+  }, [message]);
+
+  console.log(reply);
+
+  async function getReply() {
+    const docRef = doc(db, "messages", message.replyTo);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      setReply(docSnap.data());
+    }
   }
 
-  async function updateText(event){
-    event.preventDefault()
-    const text = event.target[0].value
-    const docRef = doc(db , 'messages' , message.id)
-    const newPost = {
-      text:text,
+  useEffect(() => {
+    if (copied) {
+      setTimeout(() => {
+        setCopied(false);
+      }, 2000);
     }
-    setEdit(false)
-    await updateDoc(docRef , newPost)
+  }, [copied]);
+
+  async function deleteMessage() {
+    const docRef = doc(db, "messages", message.id);
+    await deleteDoc(docRef);
+  }
+
+  async function updateText(event) {
+    event.preventDefault();
+    const text = event.target[0].value;
+    const docRef = doc(db, "messages", message.id);
+    const newPost = {
+      text: text,
+    };
+    setEdit(false);
+    await updateDoc(docRef, newPost);
+  }
+
+  function copyText() {
+    navigator.clipboard.writeText(message.text).then(() => {
+      setCopied(true);
+    });
   }
 
   return (
-    <li className={`message ${message.userId === user.uid && "message-local"}`}>
-      <div className="message-container">
+    <>
+      <li
+        className={`message ${message.userId === user.uid && "message-local"}`}
+      >
         <figure className="message--user">
           <img src={message.photoUrl} className="message--user__logo" />
-          <p className="message--user__name">{message.userName}</p>
         </figure>
-        {
-            edit ? <form onSubmit={(event) => updateText(event)}>
-              <input type="text"  className="message--content message--edit" onChange={(event) => setMsgText(event.target.value)} value={msgText} />
-            </form> :
-        <p className="message--content">{message.text}</p>
-          }
-        <div className="message--bar">
-          <button className="message--bar__btn">
-            <FontAwesomeIcon icon="fa fa-face-smile" />
-          </button>
-          <button className="message--bar__btn">
-            <FontAwesomeIcon icon="fa fa-reply" />
-          </button>
-          <button className="message--bar__btn">
-            <FontAwesomeIcon icon="fa fa-copy" />
-          </button>
-          {message.userId === user.uid && (
+        <div className="message-container">
+          <p className="message--user__name">{message.userName}</p>
+          {edit ? (
+            <form onSubmit={(event) => updateText(event)}>
+              <input
+                type="text"
+                className="message--content message--edit"
+                onChange={(event) => setMsgText(event.target.value)}
+                value={msgText}
+              />
+            </form>
+          ) : (
             <>
-              <button onClick={() => setEdit(true)} className="message--bar__btn">
-                <FontAwesomeIcon icon="fa fa-pen" />
-              </button>
-              <button onClick={deleteMessage} className="message--bar__btn">
-                <FontAwesomeIcon icon="fa fa-trash" />
-              </button>
+              {reply !== false && (
+                <div className="message--reply">
+                  <div className="message--reply-container">
+                    <p className="message--reply__name">{reply.userName}</p>
+                    <p className="message--reply__text">{reply.text}</p>
+                  </div>
+                </div>
+              )}
+              <p
+                className={`message--content ${
+                  reply && "message--content-reply"
+                }`}
+              >
+                {message.text}
+              </p>
             </>
           )}
+          <div className="message--bar">
+            <button className="message--bar__btn">
+              <FontAwesomeIcon icon="fa fa-face-smile" />
+            </button>
+            <button
+              onClick={() => replyTo(message)}
+              className="message--bar__btn"
+            >
+              <FontAwesomeIcon icon="fa fa-reply" />
+            </button>
+            <button onClick={copyText} className="message--bar__btn">
+              <FontAwesomeIcon icon="fa fa-copy" />
+            </button>
+            {message.userId === user.uid && (
+              <>
+                <button
+                  onClick={() => setEdit(true)}
+                  className="message--bar__btn"
+                >
+                  <FontAwesomeIcon icon="fa fa-pen" />
+                </button>
+                <button onClick={deleteMessage} className="message--bar__btn">
+                  <FontAwesomeIcon icon="fa fa-trash" />
+                </button>
+              </>
+            )}
+          </div>
         </div>
-      </div>
-    </li>
+      </li>
+      {copied && (
+        <div className="message--success">
+          <FontAwesomeIcon icon="fa fa-check" />
+        </div>
+      )}
+    </>
   );
 }
 
